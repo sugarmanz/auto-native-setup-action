6a7de0a0e8ba9920c64498ac6e5bead1d48ec824
Jeremiah Zucker <jeremiah@clockwyse.io>
2020-09-13 23:55:28 -0700

    fix script

--- install-linux.sh
+++ install-linux.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-if ($AUTO_VERSION == "latest") 
-wget -q "https://github.com/intuit/auto/releases/latest/download/auto-linux.gz"
+echo "Attempting to install Auto ($AUTO_VERSION)"
+
+if [ "$AUTO_VERSION" = "latest" ]; then
+  echo "Using latest"
+  URL="https://github.com/intuit/auto/releases/latest/download/auto-linux.gz"
 else
-wget -q "https://github.com/intuit/auto/releases/download/$AUTO_VERSION/auto-linux.gz"
+  echo "Specified version"
+  URL="https://github.com/intuit/auto/releases/download/$AUTO_VERSION/auto-linux.gz"
 fi
 
-gunzip -k ./auto-linux.gz
-chmod +x ./auto-linux
-mv ./auto-linux /usr/bin/auto
+if wget -q "$URL" > /dev/null; then
+  gunzip -k ./auto-linux.gz && \
+  chmod +x ./auto-linux && \
+  mv ./auto-linux /usr/local/bin/auto
+else
+  echo "Failed to download Auto binary: $URL"
+  exit 1
+fi
